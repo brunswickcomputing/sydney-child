36fb1c830162ab839e06093affc90611c8d20805
Paul Moffat <80463873+brunswickcomputing@users.noreply.github.com>
2025-07-24 19:45:59 +0100

    fix code warnings

--- header-basic.php
+++ header-basic.php
@@ -2,17 +2,18 @@
 /**
  * The header for our theme.
  *
- * Displays all of the <head> section and everything up till <div id="content">
+ * Displays all the <head> section and everything up till <div id="content">
  *
  * @package Sydney
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> lang="en-GB">
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <title></title>
 		<?php wp_head(); ?>
 	</head>
 	<body class="page--basic">
